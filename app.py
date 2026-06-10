f3e7f391c798f4e67b295d6039954c8e939fa614
JohnnyOneeye <theonlywayoutisin1@gmail.com>
2026-06-10 20:49:09 +0100

    Add user input for personalized greetingprint("Hello " + name)
    The commit changed the program by adding user input functionality, allowing the user to enter their name and receive a personalized greeting.

--- app.py
+++ app.py
@@ -1,1 +1,6 @@
 print("Hello World")
+The commit changed the program by adding user input functionality,
+allowing the user to enter their name and receive a personalized greeting.
+commit modified the program so that it prints a personalized greeting using the user's name. The line:
+
+print("Hello " + name)
